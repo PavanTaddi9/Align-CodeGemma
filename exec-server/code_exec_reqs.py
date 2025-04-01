8ca846089525de0282d56184569d7ea682e6d3ff
PavanTaddi9 <taddipavan980@gmail.com>
2025-04-01 15:43:56 +0100

    modified code exec file

--- exec-server/code_exec_reqs.py
+++ exec-server/code_exec_reqs.py
@@ -1,7 +1,27 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import requests
 import json
 import threading
+
+
+def run_coverage(server, code, tests, timeout=60, timeout_on_client=False) -> int:
+    """
+    Executes a code snippet and tests it with a set of tests,
+    then returns the coverage percentage using coverage.py.
+    """
+    tests_str = "\n".join(tests)
+    code_with_tests = code + "\n\n" + tests_str
+    data = json.dumps({"code": code_with_tests, "timeout": timeout})
+    try:
+        r = requests.post(
+            server + "/py_coverage",
+            data=data,
+            timeout=(timeout + 20) if timeout_on_client else None
+        )
+        return int(r.text)
+    except Exception as e:
+        print(e)
+        return -3
 
 
 def run_coverage_batched(server, codes, tests, timeout=60, timeout_on_client=False) -> List[int]:
@@ -13,31 +33,22 @@ def run_coverage_batched(server, codes, tests, timeout=60, timeout_on_client=Fal
     results: List[Optional[int]] = [None] * len(codes)
 
     def run_coverage_threaded(i, code, test):
-        data = json.dumps({"code": code + "\n\n" + "\n".join(test), "timeout": timeout})
-        try:
-            r = requests.post(
-                server + "/py_coverage",
-                data=data,
-                timeout=(timeout + 20) if timeout_on_client else None
-            )
-            results[i] = int(r.text)
-        except Exception as e:
-            results[i] = -3  # In case of an error during the coverage check
-            print(f"Error executing coverage for test {i}: {str(e)}")
+        results[i] = run_coverage(
+            server, code, test, timeout, timeout_on_client)
 
     for i, (code, test) in enumerate(zip(codes, tests)):
-        t = threading.Thread(target=run_coverage_threaded, args=(i, code, test))
+        t = threading.Thread(target=run_coverage_threaded,
+                             args=(i, code, test))
         threads.append(t)
         t.start()
 
     for t in threads:
         t.join()
 
-    
     results_new = []
     for r in results:
         if r is None:
-            results_new.append(-3)  
+            results_new.append((False, "Failed to execute program"))
         else:
             results_new.append(r)
 
