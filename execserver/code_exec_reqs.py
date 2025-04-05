0f0cf543bc0c636df055247ae13e880703cf14df
PavanTaddi9 <taddipavan980@gmail.com>
2025-04-05 02:22:02 +0100

    updated server

--- execserver/code_exec_reqs.py
+++ execserver/code_exec_reqs.py
@@ -4,6 +4,108 @@ import json
 import threading
 
 
+def exec_test(server, code, test, timeout=30, timeout_on_client=False, stdin="") -> Tuple[bool, str]:
+    """
+    Executes a test against a code snippet.
+    Produces true if the test passes, false otherwise.
+    Also returns the output of the code (sterr if it fails, stdout if it passes).
+
+    You can set test to an empty string if you want to execute the code without any tests
+    and just check if it runs without errors.
+
+    timeout_on_client: If true, the client will timeout after timeout+2 seconds.
+    """
+    code_with_tests = code + "\n\n" + test
+    data = json.dumps(
+        {"code": code_with_tests, "timeout": timeout, "stdin": stdin})
+    try:
+        r = requests.post(
+            server + "/py_exec",
+            data=data,
+            timeout=(timeout + 2) if timeout_on_client else None
+        )
+        lines = r.text.split("\n")
+        resp = lines[0]
+        outs = "\n".join(lines[1:])
+        assert resp == "0" or resp == "1"
+        return resp == "0", outs
+    except Exception as e:
+        return False, "Failed to execute program: " + str(e)
+
+
+def exec_test_multipl_e(server, code, test, lang, timeout=30, timeout_on_client=False) -> Tuple[bool, str]:
+    code_with_tests = code + "\n\n" + test
+    data = json.dumps(
+        {"code": code_with_tests, "lang": lang, "timeout": timeout})
+    try:
+        r = requests.post(
+            server + "/any_exec",
+            data=data,
+            timeout=(timeout + 2) if timeout_on_client else None
+        )
+        lines = r.text.split("\n")
+        resp = lines[0]
+        outs = "\n".join(lines[1:])
+        assert resp == "0" or resp == "1"
+        if outs.strip() == "Timeout":
+            return False, "Timeout"
+        # parse json
+        try:
+            outs = json.loads(outs)
+        except Exception as e:
+            return False, "Failed to parse output: " + str(e)
+
+        # get real status code
+        resp = outs["exit_code"]
+        outs = outs["stdout"] if resp == 0 else outs["stderr"]
+        return resp == 0, outs
+    except Exception as e:
+        print(e)
+        return False, "Failed to execute program: " + str(e)
+
+
+def exec_test_batched(server, codes, tests, lang=None, timeout=30, timeout_on_client=False, stdins=None) -> List[Tuple[bool, str]]:
+    """
+    Executes a batch of tests against a batch of code snippets using threading.
+
+    Lang defaults to python if not provided.
+    """
+    threads = []
+    results: List[Optional[Tuple[bool, str]]] = [None] * len(codes)
+
+    if lang and lang != "python":
+        assert stdins is None, "stdins are not supported for non-python languages for now"
+
+        def exec_fn(code, test, _): return exec_test_multipl_e(
+            server, code, test, lang, timeout, timeout_on_client)
+    else:
+        def exec_fn(code, test, stdin): return exec_test(
+            server, code, test, timeout, timeout_on_client, stdin=stdin)
+
+    def exec_test_threaded(i, code, test, stdin):
+        results[i] = exec_fn(code, test, stdin)
+
+    stdins = stdins or [None] * len(codes)
+
+    for i, (code, test, stdin) in enumerate(zip(codes, tests, stdins)):
+        t = threading.Thread(target=exec_test_threaded,
+                             args=(i, code, test, stdin))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join(timeout=timeout*2)
+
+    results_new = []
+    for r in results:
+        if r is None:
+            results_new.append((False, "Failed to execute program"))
+        else:
+            results_new.append(r)
+
+    return results_new
+
+
 def run_coverage(server, code, tests, timeout=60, timeout_on_client=False) -> int:
     """
     Executes a code snippet and tests it with a set of tests,
@@ -19,6 +121,7 @@ def run_coverage(server, code, tests, timeout=60, timeout_on_client=False) -> in
             timeout=(timeout + 20) if timeout_on_client else None
         )
         return int(r.text)
+        
     except Exception as e:
         print(e)
         return -3
--- execserver/src/main.rs
+++ execserver/src/main.rs
@@ -1,11 +1,31 @@
-use axum::{routing::post, Router, Json};
-use serde::Deserialize;
-use std::{process::Output, time::Duration};
-use tokio::io::AsyncWriteExt;
+use axum::{routing::post, Router};
+use lazy_static::lazy_static;
+use std::{
+    process::Output,
+    sync::atomic::{AtomicUsize, Ordering},
+    time::Duration,
+};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        if cfg!(debug_assertions) {
+            println!($($arg)*);
+        }
+    };
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/py_exec", post(py_exec));
+    debug!(
+        "memory limit: {} bytes (GB: {})",
+        *MEMORY_LIMIT,
+        *MEMORY_LIMIT / 1024 / 1024
+    );
+    let app = Router::new()
+        .route("/py_exec", post(py_exec))
+        .route("/any_exec", post(any_exec))
+        .route("/py_coverage", post(coverage));
 
     axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
         .serve(app.into_make_service())
@@ -13,46 +33,236 @@ async fn main() {
         .unwrap();
 }
 
-#[derive(Deserialize)]
-struct ExecRequest {
-    code: String,
-    timeout: u64,
-    stdin: Option<String>,
+lazy_static! {
+    static ref FILE_IDX: AtomicUsize = AtomicUsize::new(0);
+    static ref CPUS_AVAILABLE: usize = std::thread::available_parallelism().unwrap().into();
+    static ref CPU_SEMAPHORE: tokio::sync::Semaphore =
+        tokio::sync::Semaphore::new(*CPUS_AVAILABLE);
+    static ref CRATE_DIR: String =
+        std::env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| ".".to_string());
+    // this is total ram / cpu count. this is in kilobytes
+    static ref MEMORY_LIMIT: usize = {
+        let mem = sys_info::mem_info().unwrap().total as usize;
+        let cpus = *CPUS_AVAILABLE;
+        mem / cpus
+    };
 }
 
-async fn py_exec(Json(req): Json<ExecRequest>) -> String {
-    let stdin = req.stdin.unwrap_or_default();
-    run_python_code(&req.code, req.timeout, &stdin).await
-}
-
-async fn run_python_code(code: &str, timeout: u64, stdin: &str) -> String {
-    let output = execute_with_timeout(code, stdin, Duration::from_secs(timeout)).await;
-    format_output(output)
-}
-
-async fn execute_with_timeout(code: &str, stdin: &str, timeout: Duration) -> Option<Output> {
-    let mut child = tokio::process::Command::new("python3")
-        .arg("-c")
-        .arg(code)
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::piped())
-        .stderr(std::process::Stdio::piped())
-        .spawn()
-        .ok()?;
-
-    if !stdin.is_empty() {
-        let mut stdin_writer = child.stdin.take().unwrap();
-        stdin_writer.write_all(stdin.as_bytes()).await.ok()?;
+async fn create_temp_file(ext: &str) -> String {
+    let idx = FILE_IDX.fetch_add(1, Ordering::SeqCst);
+    // temp dir
+    let temp_dir = std::env::temp_dir().join("codeexec");
+    if !temp_dir.exists() {
+        tokio::fs::create_dir_all(&temp_dir).await.unwrap();
     }
-
-    let output = tokio::time::timeout(timeout, child.wait_with_output()).await;
-    output.ok()?
+    let filename = format!("{}/{}.{}", temp_dir.to_string_lossy(), idx, ext);
+    filename
 }
 
-fn format_output(output: Option<Output>) -> String {
+async fn run_program_with_timeout(
+    program: &str,
+    args: &[&str],
+    stdin_data: &[u8],
+    timeout: Duration,
+) -> Option<Output> {
+    let _permit = CPU_SEMAPHORE.acquire().await.unwrap();
+    let mut child = unsafe {
+        tokio::process::Command::new(program)
+            .args(args)
+            .stdout(std::process::Stdio::piped())
+            .stderr(std::process::Stdio::piped())
+            .stdin(std::process::Stdio::piped())
+            // NOTE: this is the unsafe bit
+            .pre_exec(move || {
+                // restrict gid and uid
+                nix::unistd::setgid(nix::unistd::Gid::from_raw(1000))?;
+                nix::unistd::setuid(nix::unistd::Uid::from_raw(1000))?;
+                Ok(())
+            })
+            .spawn()
+            .ok()?
+    };
+    if !stdin_data.is_empty() {
+        let stdin = child.stdin.as_mut().unwrap();
+        stdin.write_all(stdin_data).await.ok()?;
+    }
+    let output = tokio::time::timeout(timeout, child.wait()).await;
+    let mut stdout = child.stdout.take()?;
+    let mut stderr = child.stderr.take()?;
+    let mut stdout_buf = Vec::new();
+    let mut stderr_buf = Vec::new();
     match output {
-        Some(out) if out.status.success() => format!("0\n{}", String::from_utf8_lossy(&out.stdout)),
-        Some(out) => format!("1\n{}", String::from_utf8_lossy(&out.stderr)),
-        None => "1\nTimeout".to_string(),
+        Ok(output) => match output {
+            Ok(output) => {
+                stdout.read_to_end(&mut stdout_buf).await.ok()?;
+                stderr.read_to_end(&mut stderr_buf).await.ok()?;
+                Some(std::process::Output {
+                    status: output,
+                    stdout: stdout_buf,
+                    stderr: stderr_buf,
+                })
+            }
+            Err(_) => {
+                child.kill().await.ok();
+                None
+            }
+        },
+        Err(_) => {
+            child.kill().await.ok();
+            None
+        }
     }
+}
+
+fn out_to_res(output: Option<Output>) -> String {
+    match output.as_ref().map(|o| o.status.code().unwrap_or(-1)) {
+        Some(0) => format!("0\n{}", String::from_utf8_lossy(&output.unwrap().stdout)),
+        Some(-1) => "1\nTimeout".to_string(),
+        _ => format!(
+            "1\n{}",
+            output
+                .map(|o| String::from_utf8_lossy(&o.stderr).to_string())
+                .unwrap_or_default(),
+        ),
+    }
+}
+
+async fn run_py_code(code: &str, timeout: u64, stdin: String) -> (String, String) {
+    let tempfile = create_temp_file("py").await;
+    tokio::fs::write(&tempfile, code).await.unwrap();
+    let output = run_program_with_timeout(
+        "bash",
+        &[
+            "-c",
+            &format!("ulimit -v {}; python3 {}", *MEMORY_LIMIT, tempfile),
+        ],
+        stdin.as_bytes(),
+        Duration::from_secs(timeout),
+    )
+    .await;
+
+    let res = out_to_res(output);
+
+    debug!("{}: {}", tempfile, res);
+    (res, tempfile)
+}
+
+async fn run_multipl_e_prog(code: &str, lang: &str, timeout: u64) -> (String, String) {
+    let tempfile = create_temp_file(lang).await;
+    tokio::fs::write(&tempfile, code).await.unwrap();
+
+    // method:
+    // cwd into $CRATE_DIR/MultiPL-E/evaluation/src
+    // run `python3 -c "import eval_$lang; eval_$lang.eval_script('$tempfile')"`
+    let output = run_program_with_timeout(
+        "python3",
+        &[
+            "-c",
+            &format!(
+                "import sys; sys.path.append('{}/MultiPL-E/evaluation/src'); import json; import eval_{}; print(json.dumps(eval_{}.eval_script('{}')))",
+                *CRATE_DIR, lang, lang, tempfile
+            ),
+        ],
+        &[], // TODO: add stdin opt for multipl-e
+        Duration::from_secs(timeout),
+    ).await;
+    let res = out_to_res(output);
+
+    debug!("{}: {}", tempfile, res);
+    (res, tempfile)
+}
+
+/// hacky but i'm lazy
+fn get_string_json(json: &str, key: &str) -> String {
+    serde_json::from_str::<serde_json::Value>(json)
+        .map(|v| {
+            v.get(key)
+                .unwrap_or(&serde_json::Value::Null)
+                .as_str()
+                .unwrap_or("")
+                .to_string()
+        })
+        .unwrap_or_default()
+}
+
+fn get_int_json(json: &str, key: &str) -> i64 {
+    serde_json::from_str::<serde_json::Value>(json)
+        .map(|v| {
+            v.get(key)
+                .unwrap_or(&serde_json::Value::Null)
+                .as_i64()
+                .unwrap_or(0)
+        })
+        .unwrap_or(0)
+}
+
+async fn coverage(json: String) -> String {
+    let code = get_string_json(&json, "code");
+    let timeout: u64 = get_int_json(&json, "timeout") as u64;
+    let tempfile = create_temp_file("py").await;
+    tokio::fs::write(&tempfile, code).await.unwrap();
+    let cov_file = format!("{}.cov", tempfile);
+    let thunk = async {
+        let output = run_program_with_timeout(
+            "coverage",
+            &["run", "--data-file", cov_file.as_str(), tempfile.as_str()],
+            &[], // no stdin
+            Duration::from_secs(timeout),
+        )
+        .await?;
+        if output.status.code()? != 0 {
+            return None;
+        }
+        let output = run_program_with_timeout(
+            "coverage",
+            &["report", "--data-file", cov_file.as_str()],
+            &[], // no stdin
+            Duration::from_secs(10),
+        )
+        .await?;
+        if output.status.code()? != 0 {
+            return None;
+        }
+        let stdout = String::from_utf8(output.stdout).ok()?;
+        let mut cov_percentage: u8 = 0;
+        let mut next_is_cov = false;
+        for line in stdout.lines() {
+            if next_is_cov {
+                let spacesplit = line
+                    .split(' ')
+                    .map(|s| s.trim())
+                    .filter(|s| !s.is_empty())
+                    .map(|s| s.trim_end_matches('%'))
+                    .collect::<Vec<_>>();
+                cov_percentage = spacesplit.get(3)?.parse().ok()?;
+                break;
+            } else if line.starts_with("---------") {
+                next_is_cov = true;
+            }
+        }
+
+        Some(cov_percentage.to_string())
+    };
+    let res = thunk.await.unwrap_or("-1".to_string());
+    tokio::fs::remove_file(&tempfile).await.unwrap();
+    tokio::fs::remove_file(&cov_file).await.ok(); // the file may not exist
+    res
+}
+
+async fn py_exec(json: String) -> String {
+    let code = get_string_json(&json, "code");
+    let timeout: u64 = get_int_json(&json, "timeout") as u64;
+    let stdin = get_string_json(&json, "stdin");
+    let (res, tempfile) = run_py_code(&code, timeout, stdin).await;
+    tokio::fs::remove_file(&tempfile).await.unwrap();
+    res
+}
+
+async fn any_exec(json: String) -> String {
+    let code = get_string_json(&json, "code");
+    let lang = get_string_json(&json, "lang");
+    let timeout: u64 = get_int_json(&json, "timeout") as u64;
+    let (res, tempfile) = run_multipl_e_prog(&code, &lang, timeout).await;
+    tokio::fs::remove_file(&tempfile).await.unwrap();
+    res
 }
